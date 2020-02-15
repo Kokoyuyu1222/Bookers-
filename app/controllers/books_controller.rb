a2d223970b915a868a045cea66740b376fb468b0
nagoyuu <yk.rk1222@gmail.com>
2020-02-15 12:39:34 +0900

    [課題提出]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,43 +7,43 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-   @book.user = current_user
-   if @book.save
+    @book.user = current_user
+    if @book.save
      flash[:notice] = "You have creatad book successfully."
-    redirect_to book_path(@book.id)
-  else
-   render :index
+     redirect_to book_path(@book.id)
+   else
+     render :index
+   end
  end
-end
 
-def show
- @book_new = Book.new
- @book = Book.find(params[:id])
- @user = User.find(@book.user_id)
-end
+ def show
+   @book_new = Book.new
+   @book = Book.find(params[:id])
+   @user = User.find(@book.user_id)
+ end
 
-def edit
- @book = Book.find(params[:id])
-end
+ def edit
+   @book = Book.find(params[:id])
+ end
 
-def update
- @book = Book.find(params[:id])
- if @book.update(book_params)
-   flash[:notice] = "You have updated book successfully."
+ def update
+   @book = Book.find(params[:id])
+   if @book.update(book_params)
+     flash[:notice] = "You have updated book successfully."
+     redirect_to books_path
+   else
+     render :edit
+   end
+ end
+
+ def destroy
+   book = Book.find(params[:id])
+   book.destroy
    redirect_to books_path
- else
-   render :edit
  end
-end
+ private
 
-def destroy
- book = Book.find(params[:id])
- book.destroy
- redirect_to books_path
-end
-private
-
-def book_params
+ def book_params
   params.require(:book).permit(:title, :body)
 end
 def user_params
